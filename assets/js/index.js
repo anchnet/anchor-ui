2c6ca93b407e33b8dd283709024f657cf5993ce3
刘志猛 <liuzhimeng@LymanDeMacBook-Pro.local>
2017-08-17 14:56:06 +0800

    增加 bootstrap-select 插件

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,5 @@
 import bootstrap from '../bootstrap/javascripts/bootstrap'
-import bootstrapSelect from '../libs/bootstrap-select/bootstrap-select.min.js'
+import bootstrapSelect from '../libs/bootstrap-select/bootstrap-select.js'
 import Sidebar from './src/sidebar'
 
 !function ($) {
--- examples/libs/utils.js
+++ examples/libs/utils.js
@@ -2,11 +2,17 @@ import escape from './escape'
 
 export default {
   generateCode () {
-    $('.bs-example').each((key, dom) => {
-      let codeHtml = escape.decode($(dom).html())
-      $(dom).next('figure.highlight').find('code').html(codeHtml)
+    $('.bs-example').each(function (key, dom) {
+      if (!$(this).attr('data-noGenerated')) {
+        let codeHtml = escape.decode($(dom).html())
+        $(dom).next('figure.highlight').find('code').html(codeHtml)
+      }
     })
-    $('figure.highlight code').each(function(i, block) {
+    $('figure.highlight code').each(function (i, block) {
+      if ($(this).attr('data-decode')) {
+        let codeHtml = escape.decode($(block).html())
+        $(block).html(codeHtml)
+      }
       hljs.highlightBlock(block)
     })
   }
