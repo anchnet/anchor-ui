ac17249d1d4d5b2a8e9e51e253bbf6fc93b77c6b
戴立勤 <dailq@51idc.com>
2017-08-17 18:20:19 +0800

    utils增加getHtml方法

--- examples/js/src/layout.js
+++ examples/js/src/layout.js
@@ -2,42 +2,33 @@ import utils from 'examples/libs/utils'
 
 /* 插入代码 */
 const insertCode = (code) => `<pre><code>${code}</code></pre>`
-const getHtml = (el) => {
-  let $el = $(el).clone()
-  $el.find('.menu-group').removeAttr('style')
-  $el.find('.menu-dot').remove()
-  $el.find('.menu-triangle').remove()
-  let $div = $('<div>').append($el)
-  return utils.parseHtmlCode($div.html())
-}
+
+/* 布局模板 */
+const layoutTemplate = `
+  <div class="app-sidebar" data-toggle="sidebar">
+      ...
+  </div>
+  <div class="app-main">
+      <div class="app-header">
+          ...
+      </div>
+      <div class="app-body">
+          <div class="app-block">
+              ...
+          </div>
+      </div>
+  </div>
+`
 
 $(document).ready(function () {
-  /* 布局模板 */
-  let layoutTemplate = `
-    <body style="height: 100%; overflow: hidden;">
-        <div class="app-sidebar" data-toggle="sidebar">
-            ...
-        </div>
-        <div class="app-main">
-            <div class="app-header">
-                ...
-            </div>
-            <div class="app-body">
-                <div class="app-block">
-                    ...
-                </div>
-            </div>
-        </div>
-    </body>
-  `
-
   /* 插入代码 */
   $('#layoutCode').html(insertCode(utils.parseHtmlCode(layoutTemplate)))
-  $('#sidebarCode').html(insertCode(getHtml('.app-sidebar')))
-  $('#headerCode').html(insertCode(getHtml('.app-header')))
+  $('#sidebarCode').html(insertCode(utils.getHtml('.app-sidebar')))
+  $('#headerCode').html(insertCode(utils.getHtml('.app-header')))
 
   /* 代码高亮初始化 */
   $('figure.highlight code').each(function(i, block) {
     hljs.highlightBlock(block)
   })
 })
+
