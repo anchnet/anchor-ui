df1acbad706ade24f26ad6c1d77a05c2f9d8c855
戴立勤 <dailq@51idc.com>
2017-08-17 15:24:57 +0800

    优化html转换函数&修正npm包版本图标

--- examples/libs/utils.js
+++ examples/libs/utils.js
@@ -11,12 +11,28 @@ export default {
     })
   },
 
+  /* 将html转化为code并美化 */
   parseHtmlCode (html) {
     let code = html
+
+    // base64图片替换为...
     code = code.replace(/data:image\/svg\+xml;base64[^"]+/g, '...')
+
+    // 删掉空行和整段末尾的空格
     code = code.replace(/^\s*[\r\n]/gm, '').replace(/^\s*$/gm, '')
-    let spaceLen = code.match(/^\s*/)[0].length
+
+    let codeArr = code.split(/\r?\n/)
+
+    // 首行的开头空格数
+    let firstLineSpaceLen = code.match(/^\s*/)[0].length
+
+    // 末行的开头空格数
+    let lastLineSpaceLen = codeArr[codeArr.length - 1].match(/^\s*/)[0].length
+
+    // 取较大的空格数并删除每行开头的此数量的空格
+    let spaceLen = Math.max(firstLineSpaceLen, lastLineSpaceLen)
     code = code.replace(new RegExp(`^\\s{${spaceLen}}`, 'gm'), '')
+
     code = escape.decode(code)
     return code
   }
