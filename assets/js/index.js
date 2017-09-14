d017409afda89d468f0fc3ffdf19a00621e71661
刘志猛 <liuzhimeng@LymanDeMacBook-Pro.local>
2017-09-14 14:18:14 +0800

    增加控制台模板样式

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,13 @@
 import '../bootstrap/javascripts/bootstrap'
 import '../libs/bootstrap-select/bootstrap-select'
 import '../libs/bootstrap-tagsinput/bootstrap-tagsinput'
-import datetimepicker from '../libs/bootstrap-datetimepicker/bootstrap-datetimepicker.min'
 import '../libs/jquery.cookie/jquery.cookie'
 import './src/sidebar'
 import './src/tablefilter'
 import './src/transfer'
 import './src/table'
+
+import datetimepicker from '../libs/bootstrap-datetimepicker/bootstrap-datetimepicker.min'
 
 !function ($) {
   $.fn.reverse = [].reverse
