e71aaf1c29fffadc84486aed3490ced224d8e93b
戴立勤 <dailq@anchnet.com>
2017-09-14 17:15:25 +0800

    从anchor-ui中剔除jquery和moment

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,7 @@
 import '../bootstrap/javascripts/bootstrap'
 import '../libs/bootstrap-select/bootstrap-select'
 import '../libs/bootstrap-tagsinput/bootstrap-tagsinput'
-import datetimepicker from '../libs/bootstrap-datetimepicker/bootstrap-datetimepicker.min'
+import '../libs/bootstrap-datetimepicker/bootstrap-datetimepicker.min' // 源码（引用方式部分）有修改，更新需注意！
 import '../libs/jquery.cookie/jquery.cookie'
 import './src/sidebar'
 import './src/tablefilter'
@@ -10,7 +10,6 @@ import './src/table'
 
 !function ($) {
   $.fn.reverse = [].reverse
-  $.fn.datetimepicker = datetimepicker
 
   $.extend({
     ANCHOR_VERSION: '0.3.0'
